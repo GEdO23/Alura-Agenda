148e04ea6f1c1b625fd9c181aba9d34661ef77fa
GEdO23 <rm99632@fiap.com.br>
2024-11-07 09:33:38 -0300

    Utilizando AdapterViews para coleções de views

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -3,6 +3,9 @@ package br.com.alura.agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -19,12 +22,12 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         List<String> alunos = new ArrayList<>(
-                Arrays.asList("Alex", "Fran", "Jose"));
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView3);
-        TextView terceiroAluno = findViewById(R.id.textView4);
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+                Arrays.asList("Alex", "Fran", "Jose", "Maria", "Ana"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_aluno);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos
+        ));
     }
 }
