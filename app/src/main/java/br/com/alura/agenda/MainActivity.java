55ce6fe74befefca9166564e934319e9172f9767
GEdO23 <rm99632@fiap.com.br>
2024-11-07 09:24:28 -0300

    Iniciando a implementação de layout de lista

--- app/src/main/java/br/com/alura/agenda/MainActivity.java
+++ app/src/main/java/br/com/alura/agenda/MainActivity.java
@@ -2,8 +2,15 @@ package br.com.alura.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +18,13 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Alex", "Fran", "Jose"));
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView3);
+        TextView terceiroAluno = findViewById(R.id.textView4);
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
