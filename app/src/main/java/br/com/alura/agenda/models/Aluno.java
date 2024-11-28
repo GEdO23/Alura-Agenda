72149a5b0e22f851befa8b1d688798a43cdead04
GEdO23 <rm99632@fiap.com.br>
2024-11-28 12:19:40 -0300

    Editando aluno

--- app/src/main/java/br/com/alura/agenda/models/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/models/Aluno.java
@@ -5,14 +5,20 @@ import androidx.annotation.NonNull;
 import java.io.Serializable;
 
 public class Aluno implements Serializable {
-    private final String nome;
-    private final String telefone;
-    private final String email;
+
+    private int id = 0;
+    private String nome;
+    private String telefone;
+    private  String email;
 
     public Aluno(String nome, String telefone, String email) {
         this.nome = nome;
         this.telefone = telefone;
         this.email = email;
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getNome() {
@@ -25,6 +31,22 @@ public class Aluno implements Serializable {
 
     public String getEmail() {
         return email;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     @NonNull
