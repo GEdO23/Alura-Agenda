650bb9bf1bf98de276798537f751c1134bf17d1d
GEdO23 <rm99632@fiap.com.br>
2024-12-10 15:16:22 -0300

    Utilização de tipos não primitivos

--- app/src/main/java/br/com/alura/agenda/models/Aluno.java
+++ app/src/main/java/br/com/alura/agenda/models/Aluno.java
@@ -6,6 +6,7 @@ import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
 import java.io.Serializable;
+import java.util.Calendar;
 
 @Entity
 public class Aluno implements Serializable {
@@ -15,6 +16,16 @@ public class Aluno implements Serializable {
     private String nome;
     private String telefone;
     private String email;
+    
+    private Calendar momentoDeCadastro = Calendar.getInstance();
+
+    public Calendar getMomentoDeCadastro() {
+        return momentoDeCadastro;
+    }
+
+    public void setMomentoDeCadastro(Calendar momentoDeCadastro) {
+        this.momentoDeCadastro = momentoDeCadastro;
+    }
 
     @Ignore
     public Aluno(String nome, String telefone, String email) {
